d293ad1389fbd2010d15df4ff144c7225f385e51
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-16 01:13:08 -0700

    fxp webutil

---
+++ packages.lisp
@@ -0,0 +1,26 @@
+(defpackage #:erjoalgo-webutil
+  (:use :cl :cl-markup)
+  (:export #:read-file
+           #:retry-times
+           #:assoq
+           #:make-from-json-alist
+           #:->
+           #:with-json-paths
+           #:lisp-to-json-key
+           #:json-key-to-lisp
+           #:params
+           #:json-path-split
+           #:json-get-nested
+           #:json-get-nested-macro))
+
+'(:export #:make-oauth-client-from-file
+  #:OAUTH-TOKEN-AUTH-HEADER
+  #:AUTH-SERVER-REDIRECT-URL
+  #:EXCHANGE-CODE-FOR-TOKEN
+  #:RESP-TOKEN-ACCESS-TOKEN
+  #:RESP-TOKEN-REFRESH-TOKEN)
+
+'(:export #:make-api-login)
+
+(defpackage #:erjoalgo-webutil/google
+  (:use :cl :cl-markup))
