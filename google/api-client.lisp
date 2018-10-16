3f8afad4d4a31dd79e67e0d5e9556bb09960da62
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-15 19:31:46 -0700

    try to reuse google oauth and api client generator utility

---
+++ google/api-client.lisp
@@ -0,0 +1,157 @@
+(in-package #:yt-comments/client)
+
+(defvar default-api-base-url
+  "https://www.googleapis.com/youtube/v3/")
+
+(defstruct resp-page
+  items
+  page-info
+  next-page-token
+  etag
+  kind
+  error)
+
+(defstruct api-login
+  key
+  token)
+
+(defmacro log-values (form)
+  (let ((vals-sym (gensym "vals")))
+    `(let ((,vals-sym (multiple-value-call #'list
+                        ,form)))
+       (format t "values: ~A~%" ,vals-sym)
+       (values-list ,vals-sym))))
+
+(defun alist-to-http-params (params)
+  (loop for (k . v) in params
+     as k-string = (typecase k
+                     (string k)
+                     (symbol (lisp-to-json-key k))
+                     (t (error "invalid alist key type: ~A" k)))
+     unless (assoc k-string params :test #' equal)
+     collect (cons k-string v) into params
+     finally (return params)))
+
+(defun api-req (login resource params-alist
+                &key (method :get)
+                  (depaginate-p t)
+                  (retry-count 500)
+                  (retry-delay 2)
+                  (auto-refresh-p t))
+  "retuns values: json-as-alist http-resp-code resp-string"
+  (let* ((api-base-url default-api-base-url)
+         (url (concatenate 'string api-base-url resource))
+         (params (alist-to-http-params params-alist))
+         additional-headers)
+
+    (assert (= 1 (+
+                  (if (api-login-token login) 1 0)
+                  (if (api-login-key login) 1 0))))
+
+    (if (api-login-key login)
+        (push (cons :key (api-login-key login)) params)
+        (push (oauth-token-auth-header (api-login-token login))
+              additional-headers))
+
+    (labels ((req (&optional already-refreshed-p)
+               (multiple-value-bind (octets http-code)
+                   (retry-times retry-count retry-delay
+                     (loop
+                          named annoying-NS-TRY-AGAIN-CONDITION-retry
+                        for i from 0 do
+                          (handler-case
+                              (return-from annoying-NS-TRY-AGAIN-CONDITION-retry
+                                (drakma:http-request url
+                                                     :method method
+                                                     :parameters params
+                                                     :additional-headers additional-headers))
+                            (USOCKET:NS-TRY-AGAIN-CONDITION
+                                (ex)
+                              (format nil "failed with ~A: ~A retrying ~D... ~%"
+                                      'USOCKET:NS-TRY-AGAIN-CONDITION ex i)
+                              (sleep 1)))))
+                 (if (and auto-refresh-p (= 403 http-code) (not already-refreshed-p))
+                     (progn (format t "got 403. trying to refresh..." )
+                            (req t))
+                     (let* ((string (babel:octets-to-string octets :encoding :utf-8))
+                            (json (cl-json:decode-json-from-string string)))
+                       (values json http-code string))))))
+      (if (not depaginate-p)
+          (req)
+          (loop
+             with page-token-param = (cons "pageToken" nil)
+             with total-pages = -1
+             for page-idx from 1
+
+             as resp-string = nil
+             as page = nil
+             as error = nil
+             as status-code = nil
+
+             do (multiple-value-bind (body http-code string) (req)
+                  (setf resp-string string
+                        status-code http-code
+                        ;; this may fail?
+                        page (make-from-json-alist body resp-page)
+                        error (resp-page-error page)))
+             do (format t "page: ~A/~A params: ~A~%" page-idx
+                        (ceiling total-pages)
+                        params)
+             when (and (null error)
+                        ;2xx
+                       (eq 2 (floor status-code 100)))
+             do
+               (progn
+                 (setf (cdr page-token-param)
+                       (resp-page-next-page-token page))
+
+                 (when (eq -1 total-pages)
+                   (with-json-paths (resp-page-page-info page)
+                       ((per-page "resultsPerPage")
+                        (total "totalResults"))
+                     (setf total-pages (ceiling total (if (zerop per-page) -1 1))
+                           params (cons page-token-param params)))))
+
+
+             append (resp-page-items page) into items
+
+             while (and (cdr page-token-param) (not error))
+
+             finally (progn
+                       (format t "fetched ~A items~%" (length items))
+                       (return (values items status-code resp-string error))))))))
+
+(defmacro def-api-endpoint (resource-as-sym &key defaults
+                                              (fun-sym resource-as-sym))
+  `(defun ,fun-sym (login &rest params-flat)
+     (let ((params-alist
+           (loop for (k v) on params-flat by #'cddr
+              collect (cons k v))))
+       (api-req login ,(lisp-to-json-key resource-as-sym)
+                (append params-alist ,defaults)))))
+
+(def-api-endpoint comment-threads :defaults '((:part . "snippet")
+                                              (:max-results . "100")))
+
+(def-api-endpoint subscriptions :defaults '((:part . "snippet")
+                                            (:max-results . "50")))
+
+(def-api-endpoint playlists :defaults '((:max-results . "50")))
+
+(def-api-endpoint playlist-items :defaults '((:max-results . "50")))
+
+(def-api-endpoint channels :defaults '((:max-results . "50")))
+
+(def-api-endpoint videos :defaults '((:max-results . "50")))
+
+;; due to conflict with search function...
+(def-api-endpoint search :fun-sym yt-search)
+
+(def-api-endpoint activities :defaults '((:max-results . "50")))
+
+(defun delete-comment (api-login comment-id)
+  "DELETE https://www.googleapis.com/youtube/v3/comments"
+  (api-req api-login "comments"
+           `(("id" . ,comment-id))
+           :method :delete
+           :depaginate-p nil))
---
+++ google/oauth.lisp
@@ -0,0 +1,143 @@
+(in-package #:yt-comments/oauth)
+
+(defstruct oauth-client
+  client-id
+  client-secret
+  token-uri
+  scopes
+  auth-uri
+  redirect-uris)
+
+(defun make-oauth-client-from-file (filename)
+  (let* ((top-json
+          (cl-json:decode-json-from-source filename))
+         (client-json
+          (json-get-nested-macro top-json "web"))
+         (client (make-from-json-alist client-json oauth-client)))
+    (assert top-json)
+    (assert client-json)
+    (assert (with-slots (client-id client-secret) client
+              (and client-id client-secret)))
+    client))
+
+(defun fetch-token (oauth-client redirect-uri)
+  (with-slots (client-id client-secret token-uri) oauth-client
+      (-> (drakma:http-request token-uri :parameters
+                           (params
+                            "client_id" client-id
+                            "client_secret" client-secret
+                            "redirect_uri" redirect-uri
+                            "grant_type" "authorization_code"
+                            )
+                           :WANT-STREAM t)
+          (cl-json:decode-json-from-source))))
+
+(defvar resp)
+
+'(progn
+  (let ((client (make-oauth-client-from-file "py/client_secrets.json")))
+    (setf resp (fetch-token client "localhost:4242"))))
+
+"POST /oauth2/v4/token HTTP/1.1
+Host: www.googleapis.com
+Content-Type: application/x-www-form-urlencoded
+
+code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
+client_id=your_client_id&
+client_secret=your_client_secret&
+redirect_uri=https://oauth2.example.com/code&
+grant_type=authorization_code
+"
+
+(defvar auth-server-redirect-base-url
+  "https://accounts.google.com/o/oauth2/v2/auth")
+
+(defparameter youtube-scopes
+'("https://www.googleapis.com/auth/youtube.force-ssl"))
+
+(defun auth-server-redirect-url (oauth-client redirect-uri)
+  "example
+https://accounts.google.com/o/oauth2/v2/auth?
+ scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly&
+ access-type=offline&
+ include-granted-scopes=true&
+ state=state-parameter-passthrough-value&
+ redirect-uri=http%3A%2F%2Foauth2.example.com%2Fcallback&
+ response-type=code&
+ client-id=client-id
+"
+  (with-slots (scopes client-id auth-uri) oauth-client
+    (format nil "~A?~A"
+            auth-uri
+            ;; "http://localhost:1234/"
+            (-> (params
+                 "scope" (format nil "~{~A~^ ~}"
+                                 youtube-scopes)
+                 "access_type" "online"
+                 "include_granted_scopes" "false"
+                 "redirect_uri" redirect-uri
+                 "response_type" "code"
+                 "client_id" client-id)
+                (drakma::alist-to-url-encoded-string :utf-8 'drakma:url-encode)))))
+
+(defstruct resp-token
+  access-token
+  refresh-token
+  expires-in
+  token-type
+  error
+  error-description)
+
+(defun oauth-token-auth-header (resp-token)
+  (cons :authorization
+        (format nil "~A ~A"
+                (resp-token-token-type resp-token)
+                (resp-token-access-token resp-token))))
+
+(defun exchange-code-for-token (code oauth-client)
+  "code	The authorization code returned from the initial request.
+client_id	The client ID obtained from the API Console.
+client_secret	The client secret obtained from the API Console.
+redirect_uri	One of the redirect URIs listed for your project in the API Console.
+grant_type	As defined in the OAuth 2.0 specification, this field must contain a value of authorization_code.
+"
+  '"
+The following snippet shows a sample request:
+
+POST /oauth2/v4/token HTTP/1.1
+Host: www.googleapis.com
+Content-Type: application/x-www-form-urlencoded
+
+code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
+client_id=your_client_id&
+client_secret=your_client_secret&
+redirect_uri=https://oauth2.example.com/code&
+grant_type=authorization_code"
+
+
+  '"
+(response)
+{
+\"access_token\":\"1/fFAGRNJru1FTz70BzhT3Zg\",
+\"expires_in\":3920,
+\"token_type\":\"Bearer\",
+\"refresh_token\":\"1/xEoDL4iW3cxlI7yDbSRFYNG01kVKM2C-259HOF2aQbI\"
+}
+
+"
+
+(with-slots (scopes client-id client-secret token-uri redirect-uris) oauth-client
+  (->
+   (drakma:http-request
+    token-uri
+    ;; "http://localhost:1234"
+    :method :post
+    :parameters (params
+                 "code" code
+                 "grant_type" "authorization_code"
+                 "client_secret" client-secret
+                 "redirect_uri" (car redirect-uris)
+                 "client_id" client-id)
+    :want-stream t)
+   cl-json:decode-json-from-source
+   (make-from-json-alist resp-token))))
